2018dd10fc2ee2cd5124712c208a9f9d90cfa843
Ankur Sheel <ankur.sheel@gmail.com>
2017-04-09 22:48:52 +1200

    - added option to exclude long tests or run current tests

--- c++/tests/Main.cpp
+++ c++/tests/Main.cpp
@@ -1,6 +1,15 @@
+#include <string>
 #include "gtest/gtest.h"
 int main(int argc, char** argv)
 {
   testing::InitGoogleTest(&argc, argv);
+  std::string filter;
+#if 0
+  filter += ":HorseRacing*";    // current feature
+#else
+  filter += "-*LargeDataset*";  // skip large datasets
+#endif
+
+  testing::GTEST_FLAG(filter) = filter;
   return RUN_ALL_TESTS();
 }

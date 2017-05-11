1fc57a8d8628cf91b0b76be15e486574a34cb3af
Ankur Sheel <ankur.sheel@gmail.com>
2017-05-11 15:26:24 +1200

    - made custom2dArray a templated class

--- c++/src/Common/Custom2DArray.h
+++ c++/src/Common/Custom2DArray.h
@@ -1,30 +1,100 @@
 #ifndef Custom2DArray_H__
 #define Custom2DArray_H__
 
-#include <string>
+#include <cassert>
+#include <cstring>
+#include <iomanip>
 
 namespace Common
 {
+  template <class Type>
   class cCustom2DArray
   {
   public:
     cCustom2DArray();
     ~cCustom2DArray();
 
-    int GetElement(int row, int column) const { return m_array[GetIndex(row, column)]; }
-    void SetElement(int row, int column, int value) { m_array[GetIndex(row, column)] = value; }
-    void Init(int rows, int columns);
-    void PrintArray(const std::string& first, const std::string& second);
+    void Init(int rows, int columns, bool reset = true);
+    const Type& GetElement(int row, int column) const;
+    Type& GetElement(int row, int column);
+    void SetElement(int row, int column, const Type& value);
 
   private:
     void Reset();
-    int GetIndex(int row, int column) const { return row + (m_numberOfRows * column); }
+    int GetIndex(int row, int column) const { return row * m_numberOfColumns + column; }
     int GetSize() const { return m_numberOfRows * m_numberOfColumns; }
 
   private:
-    int* m_array;
+    Type* m_array;
     int m_numberOfRows;
     int m_numberOfColumns;
   };
+
+  template <class Type>
+  cCustom2DArray<Type>::cCustom2DArray()
+    : m_array(nullptr)
+    , m_numberOfRows(0)
+    , m_numberOfColumns(0)
+  {
+  }
+
+  template <class Type>
+  cCustom2DArray<Type>::~cCustom2DArray()
+  {
+    delete[] m_array;
+    m_array = nullptr;
+  }
+
+  template <class Type>
+  void cCustom2DArray<Type>::Init(int rows, int columns, bool reset)
+  {
+    if (rows != m_numberOfRows || columns != m_numberOfColumns || !reset)
+    {
+      if (m_array != nullptr)
+      {
+        delete[] m_array;
+        m_array = nullptr;
+      }
+
+      m_numberOfRows = rows;
+      m_numberOfColumns = columns;
+
+      m_array = new Type[GetSize()];
+    }
+    if (reset)
+    {
+      Reset();
+    }
+  }
+
+  template <class Type>
+  const Type& cCustom2DArray<Type>::GetElement(int row, int column) const 
+  {
+    assert(row >= 0 && row < m_numberOfRows && column >= 0 && column < m_numberOfColumns);
+    return m_array[GetIndex(row, column)];
+  }
+
+  template <class Type>
+  Type& cCustom2DArray<Type>::GetElement(int row, int column)
+  {
+    assert(row >= 0 && row < m_numberOfRows && column >= 0 && column < m_numberOfColumns);
+    return m_array[GetIndex(row, column)];
+  }
+
+  template <class Type>
+  void cCustom2DArray<Type>::SetElement(int row, int column, const Type& value)
+  {
+    assert(row >= 0 && row < m_numberOfRows && column >= 0 && column < m_numberOfColumns);
+    m_array[GetIndex(row, column)] = value;
+  }
+
+  template <class Type>
+  void cCustom2DArray<Type>::Reset()
+  {
+    if (m_array != nullptr)
+    {
+      memset(m_array, -1, GetSize() * sizeof(Type));
+    }
+  }
 }
 #endif  // Custom2DArray_H__

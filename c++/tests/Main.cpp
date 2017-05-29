c2de5821cd79fbd53cd2708080a5b466b44b2765
Ankur Sheel <ankur.sheel@gmail.com>
2017-05-29 14:40:18 +1200

    - commenting out the filter so that large dataset tests run on CI

--- c++/tests/Main.cpp
+++ c++/tests/Main.cpp
@@ -4,12 +4,12 @@ int main(int argc, char** argv)
 {
   testing::InitGoogleTest(&argc, argv);
   std::string filter;
-#if 0
-  filter += ":HorseRacing*";    // current feature
-#else
-  filter += "-*LargeDataset*";  // skip large datasets
-#endif
+//#if 0
+//  filter += ":AsciiArt*";    // current feature
+//#else
+//  filter += "-*LargeDataset*";  // skip large datasets
+//#endif
 
-  testing::GTEST_FLAG(filter) = filter;
+   //testing::GTEST_FLAG(filter) = filter;
   return RUN_ALL_TESTS();
 }

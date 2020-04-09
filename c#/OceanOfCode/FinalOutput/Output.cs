c6e5814a123d91269471cc0675ca4b3992bc4a67
Ankur Sheel <ankur.sheel@gmail.com>
2020-04-09 18:04:44 +1200

    Initial commit

---
+++ c#/OceanOfCode/FinalOutput/Output.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+ // 09/04/2020 06:00
+
+
+namespace OceanOfCode
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ c#/OceanOfCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OceanOfCode
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}

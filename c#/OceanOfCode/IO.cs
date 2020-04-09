78bcf2d1a5bef50574c3fd7f840a4c6d767d9b78
Ankur Sheel <ankur.sheel@gmail.com>
2020-04-09 18:06:09 +1200

    refactor IO

---
+++ c#/OceanOfCode/IO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OceanOfCode
+{
+    public static class Io
+    {
+        public static void Initialize()
+        {
+            var inputs = ReadLine().Split(' ');
+            var width = int.Parse(inputs[0]);
+            var height = int.Parse(inputs[1]);
+            var myId = int.Parse(inputs[2]);
+            for (var i = 0; i < height; i++)
+            {
+                var line = ReadLine();
+            }
+        }
+
+        public static void ReadTurn()
+        {
+            var inputs = ReadLine().Split(' ');
+            var x = int.Parse(inputs[0]);
+            var y = int.Parse(inputs[1]);
+            var myLife = int.Parse(inputs[2]);
+            var oppLife = int.Parse(inputs[3]);
+            var torpedoCooldown = int.Parse(inputs[4]);
+            var sonarCooldown = int.Parse(inputs[5]);
+            var silenceCooldown = int.Parse(inputs[6]);
+            var mineCooldown = int.Parse(inputs[7]);
+            var sonarResult = ReadLine();
+            var opponentOrders = ReadLine();
+        }
+        private static string ReadLine()
+        {
+            var input = Console.ReadLine();
+            Console.Error.WriteLine(input);
+            return input;
+        }
+    }
+}
--- c#/OceanOfCode/Program.cs
+++ c#/OceanOfCode/Program.cs
@@ -6,7 +6,25 @@ namespace OceanOfCode
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Io.Initialize();
+            
+            string[] inputs;
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine("7 7");
+
+            // game loop
+            while (true)
+            {
+                Io.ReadTurn();
+
+                // Write an action using Console.WriteLine()
+                // To debug: Console.Error.WriteLine("Debug messages...");
+
+                Console.WriteLine("MOVE N TORPEDO");
+            }
         }
     }
 }
